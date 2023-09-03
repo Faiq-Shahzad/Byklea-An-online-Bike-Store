f93876e607b31baab36531c88cb803d4c9d22ba7
mubirshami <mubirshami2000@gmail.com>
2023-09-03 12:56:22 +0500

    formatted file

--- routes/verifyuser.js
+++ routes/verifyuser.js
@@ -1,23 +1,23 @@
 const router = require("express").Router();
-const {User} = require("../models/user");
-
+const { User } = require("../models/user");
 
 router.get("/:verifycode", async (req, res) => {
-	try {
-		const user = await User.findOne({ isverified:false, verifycode: req.params.verifycode });
-		if (!user) return res.status(400).send("Invalid Code");
+  try {
+    const user = await User.findOne({
+      isverified: false,
+      verifycode: req.params.verifycode,
+    });
+    if (!user) return res.status(400).send("Invalid Code");
 
-		user.isverified = true;
-        user.verifycode = "";
-		await user.save();
+    user.isverified = true;
+    user.verifycode = "";
+    await user.save();
 
-		res.status(200).send("User Verified");
-		console.log("User is verified");
-	} catch (error) {
-		res.status(500).send({ message: "Internal Server Error" });
-	}
+    res.status(200).send("User Verified");
+    console.log("User is verified");
+  } catch (error) {
+    res.status(500).send({ message: "Internal Server Error" });
+  }
 });
-
-
 
 module.exports = router;
